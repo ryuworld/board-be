338ef26612a9a88a23b1e90606ceefeb610bbc17
Yoonju Ryu <uuj92@naver.com>
2022-08-06 15:07:37 +0900

    Add Sequelize

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getSlicedData } from './jsonData';
 import { getBoardTest } from './board';
+import { User } from '../model';
 
 const router = express.Router();
 
@@ -8,8 +9,9 @@ router.get('/board/test.json', getBoardTest);
 
 router.get('/sliced_data.json', getSlicedData);
 
-router.get('/', (req, res) => {
-  res.send('Hello world!');
+router.get('/', async (req, res) => {
+  let user = (await User.findAll())[0];
+  res.send(`Hello ${user.username}!`);
 });
 
 export default router;
